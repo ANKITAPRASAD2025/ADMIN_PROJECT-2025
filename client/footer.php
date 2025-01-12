e9f53665095b88bfa6d09e08f8f128416679f53e
ANKITAPRASAD2025 <ankita.websadroit@gmail.com>
2025-01-12 22:58:21 +0530

    Add files via upload

---
+++ client/footer.php
@@ -0,0 +1,5 @@
+
+<!-- <h1>i am footer</h1> -->
+<script src="../assets/bootstrap/css/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ client/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/form.css">
+
+    <title>
+          <?= $page_title['page_title_here']?>
+   
+    </title>
+</head>
+<body>
+    <!-- <h1 class="text-primary"> header</h1> -->
+
+    
